91117c3e2905ea87bc4de5fb774f93ff6e65a75d
Nguyễn Trọng Phúc <ngtrongphuc1905@gmail.com>
2022-06-18 21:28:40 +0700

    Update header.php

--- mvc/views/blocks/header.php
+++ mvc/views/blocks/header.php
@@ -27,7 +27,7 @@ foreach ($cart as $item) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>PhoneStore</title>
+    <title>BKPhone</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
